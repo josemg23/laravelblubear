18f014b5837826b0c990fd3043d96776892da3e7
JGMS <josegri_magallanes@hotmail.com>
2024-04-13 15:37:02 -0500

    app

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://laravel-app-knmy9.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
